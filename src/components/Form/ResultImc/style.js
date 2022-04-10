df192679137cf3f369827dacf922a10e057d3516
thiagoeu <araujo.thiago1051@gmail.com>
2022-04-10 20:52:25 -0300

    add styles

--- src/components/Form/ResultImc/style.js
+++ src/components/Form/ResultImc/style.js
@@ -1,8 +1,27 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-   
-    
+    resultImc:{
+        flex: 1,
+        marginTop: 15,
+        paddingTop: 60,
+        borderRadius: 50,
+        alignItems: "center",
+        width: "100%",    
+    },
+    numberImc:{
+        fontSize: 48,
+        color: "#cc0000",
+        fontWeight: "bold",
+    },
+
+    information:{
+        fontSize: 18,
+        color: "#cc0000",
+        fontWeight: "bold",
+
+    },
+      
 });
 
 export default styles
--- src/components/Form/style.js
+++ src/components/Form/style.js
@@ -30,7 +30,23 @@ const styles = StyleSheet.create({
        height: 40,
        margin:12,
        paddingLeft: 10,
+   },
 
+   textButtonCalculator:{
+       fontSize:  20,
+       color: "#ffffff",
+
+   },
+   buttonCalculator:{
+       borderRadius: 50,
+       alignItems: "center",
+       justifyContent: "center",
+       width: "90%",
+       backgroundColor: "#cc0000",
+       paddingTop: 14,
+       paddingBottom: 14,
+       marginLeft: 12,
+       marginTop: 30, 
 
    }
 });
